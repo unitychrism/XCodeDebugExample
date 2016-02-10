e6a4108612984fdf058339dd8e46129bfb72dba2
Chris Mahoney <chrism@unity3d.com>
2016-02-10 10:42:46 -0600

    Initial commit

---
+++ Assets/Scripts/Counter.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.UI;
+
+using Random = UnityEngine.Random;
+
+public class Counter : MonoBehaviour {
+
+	int lastSecond;
+	Text counterText;
+
+	// Use this for initialization
+	void Start () {
+		lastSecond = 0;
+		counterText = GameObject.Find ("CounterText").GetComponent<Text> ();
+
+		// Throw an exception on start
+		throw new Exception ("Start counting exception!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int newSecond = (int)Time.timeSinceLevelLoad;
+
+		if (newSecond > lastSecond) {
+			// Display and log count
+			SetCount(newSecond);
+
+			// Throw exception and change bg color every 5 seconds
+			if (newSecond % 5 == 0) {
+				throw new Exception("Five seconds elapsed exception!");
+			}
+
+			lastSecond = newSecond;
+		}
+	}
+
+	public void SetCount(int count) {
+		Debug.Log (string.Format ("Tick: {0}", count));
+		counterText.text = count.ToString();
+	}
+}
